ea7a6ce8e63003bd66304c2afee8abdd122e717e
sonhavietnamese <nguyensonha.hanz@gmail.com>
2025-09-08 01:16:06 +0700

    feat(score): add 0-3 score with chestnut UI; +1 on attack, +1 on buy/drink, set to 3 on boss kill; clamp max 3

--- src/components/hud/hud.tsx
+++ src/components/hud/hud.tsx
@@ -11,21 +11,23 @@ export default function Hud() {
     bossMaxHP,
     showBuyCokeButton,
     performBuyCoke,
+    score,
   } = useFloorStore()
+
+  const slots = [0, 1, 2]
 
   return (
     <div className='fixed z-[10] pointer-events-none top-0 left-0 w-full h-full'>
       <aside className='absolute top-4 left-1/2 -translate-x-1/2 pointer-events-none'>
         <ul className='flex gap-1'>
-          <li className='w-8 aspect-square'>
-            <img src='/elements/chestnut.png' alt='Chestnut' />
-          </li>
-          <li className='w-8 aspect-square'>
-            <img src='/elements/chestnut.png' alt='Chestnut' />
-          </li>
-          <li className='w-8 aspect-square'>
-            <img src='/elements/chestnut-silhouette.png' alt='Chestnut' />
-          </li>
+          {slots.map((i) => (
+            <li key={i} className='w-8 aspect-square'>
+              <img
+                src={i < score ? '/elements/chestnut.png' : '/elements/chestnut-silhouette.png'}
+                alt='Chestnut'
+              />
+            </li>
+          ))}
         </ul>
       </aside>
 
