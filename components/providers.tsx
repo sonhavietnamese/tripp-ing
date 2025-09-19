8e24b369bd5861f4313c348ef0a1d93cd39177f0
sonhavietnamese <nguyensonha.hanz@gmail.com>
2025-09-19 11:45:24 +0700

    fix: REMOVE WALELT

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,26 +1,10 @@
 'use client'
 
 import '@/styles/globals.css'
-import { roninWallet } from '@sky-mavis/tanto-wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { saigon } from 'viem/chains'
-import { createConfig, http, WagmiProvider } from 'wagmi'
 
 const queryClient = new QueryClient()
 
-const config = createConfig({
-  chains: [saigon],
-  transports: {
-    [saigon.id]: http(),
-  },
-  multiInjectedProviderDiscovery: false,
-  connectors: [roninWallet()],
-})
-
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </WagmiProvider>
-  )
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
